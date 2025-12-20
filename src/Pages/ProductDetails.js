07418124a1cd7cdbf0d2c40d395bca148852df17
seif <seif.mohamed348@gmail.com>
2025-12-20 08:04:05 +0200

    edit status

--- src/Pages/ProductDetails.js
+++ src/Pages/ProductDetails.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import axios from "axios";
-import API from "./api";
+
 import "./ProductDetails.css";
 
 export default function ProductDetails() {
